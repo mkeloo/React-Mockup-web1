97ec54c51f11b58c94d179fc352c8c294037609e
mkeloo <mkeloo@ufl.edu>
2022-09-10 06:44:20 -0400

    first commit

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div className="intro">
+        <div className="i-left">
+            <div className="i-name">
+                <span>Hello! I Am</span>
+                <span>Mickey</span>
+                <span>Front-End Developer with high level of experience in web designing and development, producting the Quality work.</span>
+            </div>
+        </div>
+        <div className="i-right"></div>
+    </div>
+  )
+}
+ 
+export default Intro
