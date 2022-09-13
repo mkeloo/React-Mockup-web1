0e60ef631e7c925f70c0fa860edad21dbf17ee12
mkeloo <mkeloo@ufl.edu>
2022-09-12 20:46:15 -0400

    Contact and EmailJS setup done

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,10 +1,81 @@
-import React from 'react'
-import './Contact.css'
+import React from 'react';
+import './Contact.css';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  // email js functionality
+  const form = useRef();
+  const [done, setDone] = React.useState(false);
+  const [value, setValue] = React.useState();
 
-export default Contact 
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_eejk77l',
+        'template_xdszqom',
+        form.current,
+        'nc_Lw_9AeEsroJ6_L'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
+          setValue('');
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="contact-form">
+      <div className="works-left">
+        <div className="services-left">
+          <span>Get in touch</span>
+          <span>Contact me</span>
+          <div
+            className="blur s-blur1"
+            style={{ background: 'var(--purple)' }}
+          ></div>
+        </div>
+      </div>
+
+      <div className="contact-right">
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            className="user"
+            name="user_name"
+            placeholder="Name"
+            value={value}
+          />
+          <input
+            type="email"
+            className="user"
+            name="user_email"
+            placeholder="Email"
+            value={value}
+          />
+          <textarea
+            className="user"
+            name="message"
+            placeholder="Message"
+            value={value}
+          />
+          <input type="submit" value="Send" className="button" />
+          <span>{done && 'Thanks for contacting me!'}</span>
+          <div
+            className="blur c-blur1"
+            style={{ background: 'var(--skyblue)' }}
+          ></div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -1,54 +1,59 @@
-import React from 'react'
-import './Works.css'
-import Upwork from '../../img/Upwork.png'
-import Fiverr from '../../img/fiverr.png'
-import Amazon from '../../img/amazon.png'
-import Shopify from '../../img/Shopify.png'
-import Facebook from '../../img/Facebook.png'
+import React from 'react';
+import './Works.css';
+import Upwork from '../../img/Upwork.png';
+import Fiverr from '../../img/fiverr.png';
+import Amazon from '../../img/amazon.png';
+import Shopify from '../../img/Shopify.png';
+import Facebook from '../../img/Facebook.png';
 
 const Works = () => {
   return (
     <div className="works">
+      <div className="works-left">
         <div className="services-left">
-            <span>Works for All these</span>
-            <span>Brands & Clients</span>
-            <span>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique.
-                <br />
-                explicabo accusamus nulla libero tenetur enim dicta aperiam quod quia blanditiis?
-                <br />
-                Aperiam repudiandae minus temporibus voluptate!
-            </span>
-            <button className='button s-button'>Hire Me</button>
-            <div className="blur s-blur1" style={{background: "#ABF1FF94"}}></div>
+          <span>Works for All these</span>
+          <span>Brands & Clients</span>
+          <span>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique.
+            <br />
+            explicabo accusamus nulla libero tenetur enim dicta aperiam quodr   
+            quia blanditiis?
+            <br />
+            Aperiam repudiandae minus temporibus voluptate!
+          </span>
+          <button className="button s-button">Hire Me</button>
+          <div
+            className="blur s-blur1"
+            style={{ background: '#ABF1FF94' }}
+          ></div>
+        </div>
+      </div>
+
+      <div className="works-right">
+        <div className="works-main-circle">
+          <div className="works-sec-circle">
+            <img src={Upwork} alt="Upwork" />
+          </div>
+          <div className="works-sec-circle">
+            <img src={Fiverr} alt="Fiverr" />
+          </div>
+          <div className="works-sec-circle">
+            <img src={Amazon} alt="Amazon" />
+          </div>
+          <div className="works-sec-circle">
+            <img src={Shopify} alt="Shopify" />
+          </div>
+          <div className="works-sec-circle">
+            <img src={Facebook} alt="Facbook" />
+          </div>
         </div>
 
-        <div className="works-right">
-            <div className="works-main-circle">
-                <div className="works-sec-circle">
-                    <img src={Upwork} alt="Upwork" />
-                </div>
-                <div className="works-sec-circle">
-                    <img src={Fiverr} alt="Fiverr" />
-                </div>
-                <div className="works-sec-circle">
-                    <img src={Amazon} alt="Amazon" />
-                </div>
-                <div className="works-sec-circle">
-                    <img src={Shopify} alt="Shopify" />
-                </div>
-                <div className="works-sec-circle">
-                    <img src={Facebook} alt="Facbook" />
-                </div>
-            </div>
-
-            {/* Background circles */}
-            <div className="works-back-circle blue-circle"></div>
-            <div className="works-back-circle yellow-circle"></div>
-
-        </div>
+        {/* Background circles */}
+        <div className="works-back-circle blue-circle"></div>
+        <div className="works-back-circle yellow-circle"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
